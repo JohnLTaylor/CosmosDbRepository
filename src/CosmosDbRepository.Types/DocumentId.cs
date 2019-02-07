38736391d5d2d976e9701395e7d9c80fa171758a
John Taylor <john.taylor@doverfs.com>
2019-02-07 12:25:01 -0600

    Add the IEquatable operators

--- src/CosmosDbRepository.Types/DocumentId.cs
+++ src/CosmosDbRepository.Types/DocumentId.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CosmosDbRepository.Types
 {
-    public struct DocumentId
+    public struct DocumentId : IEquatable<DocumentId>
     {
         public string Id { get; }
 
@@ -24,6 +25,31 @@ namespace CosmosDbRepository.Types
         public static implicit operator DocumentId(int id)
         {
             return new DocumentId(id.ToString());
+        }
+
+        public static bool operator ==(DocumentId left, DocumentId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DocumentId left, DocumentId right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DocumentId id && Equals(id);
+        }
+
+        public bool Equals(DocumentId other)
+        {
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + EqualityComparer<string>.Default.GetHashCode(Id);
         }
     }
 }
