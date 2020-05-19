68203d88bae2634a380352c55da20d5421d86431
John Taylor <john.taylor@doverfs.com>
2020-05-18 18:28:54 -0600

    Add partition key support to stored procedures

--- src/CosmosDbRepository/ICosmosDbRepositoryExtensions.cs
+++ src/CosmosDbRepository/ICosmosDbRepositoryExtensions.cs
@@ -12,7 +12,7 @@ namespace CosmosDbRepository
 {
     public static class ICosmosDbRepositoryExtention
     {
-        public static Task<T> AddAsync<T,U>(this ICosmosDbRepository<T> repo, U partitionKey, T entity, RequestOptions requestOptions = null)
+        public static Task<T> AddAsync<T, U>(this ICosmosDbRepository<T> repo, U partitionKey, T entity, RequestOptions requestOptions = null)
         {
             requestOptions = SetPartitionKey(partitionKey, requestOptions);
             return repo.AddAsync(entity, requestOptions);
@@ -170,6 +170,102 @@ namespace CosmosDbRepository
         {
             feedOptions = SetCrossPartition(feedOptions);
             return repo.FindFirstOrDefaultAsync(predicate, clauses, feedOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, TResult>(this IStoredProcedure<T1, TResult> storedProcedure, U partitionKey, T1 t1, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, TResult>(this IStoredProcedure<T1, T2, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, TResult>(this IStoredProcedure<T1, T2, T3, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, TResult>(this IStoredProcedure<T1, T2, T3, T4, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14, T15 t15, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, requestOptions);
+        }
+
+        public static Task<TResult> ExecuteAsync<U, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this IStoredProcedure<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> storedProcedure, U partitionKey, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14, T15 t15, T16 t16, RequestOptions requestOptions = null)
+        {
+            requestOptions = SetPartitionKey(partitionKey, requestOptions);
+            return storedProcedure.ExecuteAsync(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, requestOptions);
         }
 
         private static RequestOptions SetPartitionKey<U>(U partitionKey, RequestOptions requestOptions)
