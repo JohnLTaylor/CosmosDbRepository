e68e08f479768a18b8797f46394f1e19c320b8ae
John Taylor <john.taylor@doverfs.com>
2021-05-04 08:16:46 -0500

    Add test

--- test/CosmosDbRepositoryTest/GuidId/CosmosDbRepositorySelectTests.cs
+++ test/CosmosDbRepositoryTest/GuidId/CosmosDbRepositorySelectTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Microsoft.Azure.Documents;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
@@ -117,6 +118,33 @@ namespace CosmosDbRepositoryTest.GuidId
                 var count = new[] { data1, data2a, data2b, data3a, data3b, data3c }.SelectMany(a => a.Subdata.SelectMany(b => b.SubSubData)).Count();
 
                 groupResults.Count().Should().Be(count);
+            }
+        }
+
+        [TestMethod]
+        public async Task Select_MinMax_Expect_Success()
+        {
+            using (var context = CreateContext())
+            {
+                var uniqueData = Guid.NewGuid().ToString();
+
+                var data1 = await GetTestData(context, uniqueData, 1, CreateSubData);
+                var data2a = await GetTestData(context, uniqueData, 2, CreateSubData);
+                var data2b = await GetTestData(context, uniqueData, 2, CreateSubData);
+                var data3a = await GetTestData(context, uniqueData, 3, CreateSubData);
+                var data3b = await GetTestData(context, uniqueData, 3, CreateSubData);
+                var data3c = await GetTestData(context, uniqueData, 3, CreateSubData);
+
+                var result = await context.Repo.SelectAsync<Document>($"SELECT MIN(c.rank) AS Min, MAX(c.rank) AS Max FROM c WHERE c.data = '{uniqueData}'");
+
+                result.Should().NotBeNull();
+                result.Count().Should().Be(1);
+
+                int min = result[0].GetPropertyValue<int>("Min");
+                int max = result[0].GetPropertyValue<int>("Max");
+
+                min.Should().Be(1);
+                max.Should().Be(3);
             }
         }
 
